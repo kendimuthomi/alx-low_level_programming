741c3cd3140f3516f263878d1c3e4061eb7b150f
kendimuthomi <kendijackline42@gmail.com>
2022-06-01 23:52:40 -0700

    My first C program using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** 
+ * main is the entry point
+ *
+ * description: a C program that prints using the puts function
+ *
+ * Always return 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
