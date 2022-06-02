dbd8163a7af2f7dc34675413bba351fc06ebc359
kendimuthomi <kendijackline42@gmail.com>
2022-06-02 01:11:06 -0700

    Let us check the sizes of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	printf("The size of an int is: %lu byte(s)\n", sizeof(int));
-	printf("The size of a char is: %lu byte(s)\n", sizeof(char));
-	printf("The size of a float is: %lu byte(s)\n", sizeof(float));
-	printf("The size of a double is: %lu byte(s)\n", sizeof(double));
+	printf("The size of an char is: %lu byte(s)\n", sizeof(char));
+	printf("The size of a int is: %lu byte(s)\n", sizeof(int));
 	printf("The size of a long int is: %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of long long int is: %lu byte(s)\n", sizeof(long long int));
+	printf("The size of a double is: %lu byte(s)\n", sizeof(double));
+	printf("Size of a float is: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
 
