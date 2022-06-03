b0a04bdcf72b8dd3e93e382855717340cc5e7601
kendimuthomi <kendijackline42@gmail.com>
2022-06-03 01:22:43 -0700

    A new quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
 #include<unistd.h>
-/*
- * main is the entry point
+/**
+ * main- This is the entry point
  *
  * Description: printing a quote using thw write function in C
  * write(int fd, const void, size_t count)
  *
- * Return 1(error)
+ * Return: 1(error)
  */
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote, 59);
+	write(1, q, 59);
 	return (1);
 }
