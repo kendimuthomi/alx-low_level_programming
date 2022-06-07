a5baece5212de7a314f65c9c2bcf02cc90be49e4
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2022-06-07 19:17:46 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,21 @@
  */
 void print_to_98(int n)
 {
-	int c;
-
-	if (n > 98)
-		for (c = n; c > 98; c--)
-			_putchar(c);
-	else
-		for (c = n; c < 98; c++)
-			_putchar(c);
-	_putchar(98);
-	_putchar('\n');
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
 }
