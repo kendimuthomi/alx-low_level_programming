60c45635be05ed59654d584f1403f14a020db28c
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2022-06-08 09:00:45 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,70 @@
+#include"main.h"
 #include <stdio.h>
 /**
- * main - main block
- * Description: computes and prints all the fibonacci numbers below 98
- * Return: 0
- */
+ * numLength - returns the length of string
+ *
+ * @num: operand number
+ *
+ * Return: number of digits
+*/
+
+int numLength(int n)
+{
+	int len = 0;
+
+	if (!n)
+		return (1);
+
+	while (n)
+	{
+		n = n / 10;
+		len += 1;
+	}
+
+	return (len);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the first 98 Fibonacci numbers
+ *	starting with 1 and 2 followed by a new line
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	int i = 0;
-	unsigned long int a = 0, b = 1, next = 0;
+	int count, initial0s;
+	unsigned long fib1 = 1, fib2 = 2, sum, max = 100000000, fib1o = 0, fib2o = 0, sumo = 0;
 
-	while (i < 98)
+	for (count = 1; count <= 98; ++count)
 	{
-		next = a + b;
-		a = b;
-		b = next;
-		printf("%lu", next);
+		if (fib1o > 0)
+			printf("%lu", fib1o);
+		initial0s = numLength(max) - 1 - numLength(f1);
 
-		if (i < 97)
+		while (fib1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			--initial0s;
+		}
+
+		printf("%lu", fib1);
+
+		sum = (fib1 + fib2) % max;
+		sumo = fib1o + fib2o + (fib1 + fib2) / max;
+		fib1 = fib2;
+		fib1o = fib2o;
+		fib2 = sum;
+		fib2o = sumo;
+
+		if (count != 98)
 			printf(", ");
-		i++;
+		else
+			printf("\n");
 	}
-	putchar('\n');
+
 	return (0);
 }
