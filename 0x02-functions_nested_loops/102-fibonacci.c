bb628654da220912d6d3a35951328cd834e3fc77
kendimuthomi <kendijackline42@gmail.com>
2022-06-07 21:36:52 -0700

    fibonacci numbers below 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: fibonacci numbers below 50
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+	unsigned long int num1 = 0, num2 = 1, sum;
+
+	for (c = 0; c < 50; c++)
+	{
+		sum = num1 + num2;
+		printf("%lu", sum);
+		num1 = num2;
+		num2 = sum;
+
+		if (c == 49)
+			printf("\n");
+		else
+			printf(",");
+	}
+	return (0);
+}
