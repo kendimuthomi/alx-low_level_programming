756984c45e71e5f8c80af4c6e77624ab596ace1d
kendimuthomi <kendijackline42@gmail.com>
2022-06-08 02:27:47 -0700

    checking for an infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	/*
+	 * while (i < 10)
+	 * {
+	 *	putchar(i);
+	 * }
+	 */
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Learning how to manually debug my code
