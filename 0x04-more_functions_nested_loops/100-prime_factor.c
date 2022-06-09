4d574dd65a10b94c0c60d53ceecb738bec780b3e
kendimuthomi <kendijackline42@gmail.com>
2022-06-09 04:48:40 -0700

    Prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - enrty point
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned int long num = 612852475143, z = (int) sqrt(n);
+
+	while (1)
+	{
+		if (num % z == 0)
+		{
+			printf("%lu \n", num / z);
+			break;
+		}
+		z--;
+	}
+	return (0);
+}
