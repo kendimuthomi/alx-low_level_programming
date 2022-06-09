218672208ee572042f0f92835f8ee82f876be841
kendimuthomi <kendijackline42@gmail.com>
2022-06-09 03:56:38 -0700

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,12 +5,15 @@
  */
 void print_most_numbers(void)
 {
-	int a;
+	char a = 0;
 
-	for (a = 0; a <= 9; a++)
+	while (a <= 9)
 	{
-		if (a != 2 || a != 4)
+		if (a != 2 && a != 4)
+		{
 			_putchar(a + '0');
+		}
+		a++;
 	}
 	_putchar('\n');
 }
