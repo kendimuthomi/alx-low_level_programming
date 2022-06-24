9285b293c7a2478ec34cef159c9baf3dff12e8d8
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2022-06-25 00:05:29 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,71 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 /**
- * isCoin - function that helps to calculate number of coins
- * @j: int
- * Return: number of coins needed
+ * isInteger - checks if s is an integer
+ * @s: string to check
+ * Return: 0 or 1
  */
-int isCoin(int j)
+
+int isInteger(const char *s)
 {
-	int count = 0;
-
-	while (j != 0)
-	{
-		if (j % 10 == 9 || j % 10 == 7)
-			j -= 2;
-		else if (j % 25 == 0)
-			j -= 25;
-		else if (j % 10 == 0)
-			j -= 10;
-		else if (j % 5 == 0)
-			j -= 5;
-			else if (j % 2 == 0)
-		{
-			if (j % 10 == 6)
-				j -= 1;
-			else
-				j -= 2;
-		}
-		else
-			j -= 1;
-
-		count++;
-	}
-
-	return (count);
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (0);
+	i++;
+}
+return (1);
 }
 
 /**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char const *argv[])
 {
-	int j, coin;
+int i = 0, coinUsed = 0, coin = 0;
+int coins[] = {25, 10, 5, 2, 1};
 
-	coin = 0;
-
-	if (argc != 2)
+if (argc != 2)
+{
+	printf("Error\n");
+	return (1);
+}
+if (isInteger(argv[1]))
+{
+	i = atoi(argv[1]);
+	while (i > 0 && coin <= 4)
 	{
-		printf("Error\n");
-		return (1);
+		if (i >= coins[coin])
+		{
+			i -= coins[coin];
+			coinUsed++;
+		}
+		else
+		{
+			coin++;
+		}
 	}
+}
 
-	j = atoi(argv[1]);
+printf("%i\n", coinUsed);
 
-	if (j < 0)
-		printf("0\n");
-
-	else
-	{
-		coin = isCoin(j);
-
-		printf("%d\n", coin);
-	}
-
-	return (0);
+return (0);
 }
