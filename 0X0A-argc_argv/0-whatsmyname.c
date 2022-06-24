b5f3cb788d6517c3dbd67da3b9c7d84fbfb51676
kendimuthomi <kendijackline42@gmail.com>
2022-06-24 07:53:13 -0700

    what's my name

---
+++ 0X0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: an int
+ * @argv: pointer to a list
+ * Return: 0(success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
