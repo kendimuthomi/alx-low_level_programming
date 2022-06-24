0aa35bb01a3611db18c6462d79268eb9bf451505
kendimuthomi <kendijackline42@gmail.com>
2022-06-24 08:35:30 -0700

    print all received arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of command line arguments
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int j = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[j]);
+		j++;
+	}
+	return (0);
+}
