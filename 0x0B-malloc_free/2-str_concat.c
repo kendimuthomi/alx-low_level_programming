2135db21c732949e1e4ec44dbacfecabfd69d3ed
kendimuthomi <kendijackline42@gmail.com>
2022-06-25 22:08:43 -0700

    string concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,6 +8,8 @@
  */
 int strLen(char *s)
 {
+	int size;
+
 	for (size = 0; s[size] != '\0'; size++)
 	;
 	return (size);
@@ -22,7 +24,7 @@ int strLen(char *s)
 char *str_concat(char *s1, char *s2)
 {
 	int size1, size2, j;
-	char *mem;
+	char *memory;
 
 	if (s1 == NULL)
 		s1 = '\0';
@@ -30,16 +32,16 @@ char *str_concat(char *s1, char *s2)
 		s2 = '\0';
 	size1 = strLen(s1);
 	size2 = strLen(s2);
-	mem = malloc((size1 + size2) * sizeof(char) + 1);
-	if (mem == 0)
+	memory = malloc((size1 + size2) * sizeof(char) + 1);
+	if (memory == 0)
 		return (0);
 	for (j = 0; j < size1 + size2; j++)
 	{
 		if (j < size1)
-			mem[j] = s1[j];
-		els
-			mem[j] = s2[j - size1];
+			memory[j] = s1[j];
+		else
+			memory[j] = s2[j - size1];
 	}
-	mem[j] = '\0';
-	return (mem);
+	memory[j] = '\0';
+	return (memory);
 }
