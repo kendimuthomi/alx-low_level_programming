a31a47b03c02aec072ad5fed75e3f89281ed99b4
kendimuthomi <kendijackline42@gmail.com>
2022-06-30 02:08:46 -0700

    reallocate memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -38,7 +38,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (malloc(new_size));
 	}
-	else if (new_size <= old_size)
+	else if (new_size == old_size)
 	{
 		return (ptr);
 	}
