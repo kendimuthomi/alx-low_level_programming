ca7dd8b11d33689e2a20c0143cb01d1a2f0d2878
kendimuthomi <kendijackline42@gmail.com>
2022-07-07 09:12:09 -0700

    Variadic function to print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include<stdio.h>
+#include <stdio.h>
 /**
  * print_numbers - prints numbers
  * @separator: string
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (separator != NULL || i != (n - 1))
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
