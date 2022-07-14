b1b52bbb8f14651c05f2ce0091f29cade4cb9459
kendimuthomi <kendijackline42@gmail.com>
2022-07-14 03:17:49 -0700

    Tidying up

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,15 +7,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *current;
 	list_t *next_node;
 
-	current = head;
-	while (current != NULL)
+	while (head)
 	{
-		next_node = current->next;
-		free(current->str);
-		free(current);
-		current = next_node;
+		next_node = head->next;
+		free(head->str);
+		free(head);
+		head = next_node;
 	}
 }
