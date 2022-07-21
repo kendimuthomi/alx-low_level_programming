12726fc90bab290f20be46506fd460de0ded8cfe
kendimuthomi <kendijackline42@gmail.com>
2022-07-21 02:21:25 -0700

    Binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,15 +7,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int number = 0;
-	int i = 0;
+	int i;
 
-	if (b[i] == '\0')
+	if (!b)
 		return (0);
-	while ((b[i] == '0') || (b[i] == '1'))
+	for (i = 0; b[i]; i++)
 	{
-		number <<= 1;
-		number += b[i] - '0';
-		i++;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		number = 2 * number + (b[i] - '0');
 	}
 	return (number);
 }
