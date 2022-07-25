63b85eca73689d27237c3c1a21d34697a996d666
kendimuthomi <kendijackline42@gmail.com>
2022-07-25 13:03:09 -0700

    Check if a file is an elf file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * check_elf - checks if a file is an elf
+ * @e_ident: pointer to an array that contains the elf magic number
+ */
+void check_elf(unsigned char *e_ident)
+{
+	int i;
+
+	for (i = 0; i < 4; i++)
+	{
+		if (e_ident[i] != 127 &&
+				e_ident[i] != 'E' &&
+				e_ident[i] != 'L' &&
+				e_ident[i] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
